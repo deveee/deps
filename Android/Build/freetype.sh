295fc4b8ef7660dc5d56198b8c2cb2fa8911e70c
MoNTE48 <MoNTE48@mail.ua>
2021-08-07 00:44:41 +0200

    Android: Update FreeType 2.11.0 (armeabi-v7a, arm64-v8a)

---
+++ Android/Build/freetype.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -e
+
+. sdk.sh
+FREETYPE_VERSION=2.11.0
+
+mkdir -p deps; cd deps
+
+if [ ! -d libcurl-src ]; then
+	wget https://download.savannah.gnu.org/releases/freetype/freetype-$FREETYPE_VERSION.tar.gz
+	tar -xzvf freetype-$FREETYPE_VERSION.tar.gz
+	mv freetype-$FREETYPE_VERSION freetype-src
+	rm freetype-$FREETYPE_VERSION.tar.gz
+fi
+
+cd freetype-src
+
+./configure --host=$TARGET CFLAGS="$CFLAGS -fPIC" CPPFLAGS="$CXXFLAGS -fPIC" \
+	--prefix=/ --disable-shared --enable-static \
+	--with-zlib=yes --with-brotli=no --with-bzip2=no --with-png=no --with-harfbuzz=no
+
+make -j
+
+# update `include` folder
+rm -rf ../../../Freetype/include/
+cp -r include ../../../Freetype/include
+rm -rf ../../../Freetype/include/dlg
+# update lib
+rm -rf ../../../Freetype/clang/$TARGET_ABI/libfreetype.a
+cp -r objs/.libs/libfreetype.a ../../../Freetype/clang/$TARGET_ABI/libfreetype.a
+
+echo "libcurl build successful"
