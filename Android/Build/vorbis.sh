96e8b392157a43baa9eb24db09ef6bdaf7b0a1b4
Maksim <Maksym48@pm.me>
2022-06-27 23:18:46 +0300

    Android: Update Vorbis+Ogg [master] (armeabi-v7a, arm64-v8a)

---
+++ Android/Build/vorbis.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -e
+
+. sdk.sh
+
+export ANDR_ROOT=$(pwd)
+
+mkdir -p deps; cd deps
+
+if [ ! -d vorbis-src ]; then
+	git clone https://github.com/MoNTE48/libvorbis-android vorbis-src
+fi
+
+cd vorbis-src
+
+$ANDROID_NDK/ndk-build \
+ APP_PLATFORM=android-"$API" \
+ TARGET_ABI="$TARGET_ABI" \
+ NDK_APPLICATION_MK="$ANDR_ROOT/Deps.mk"
+
+# update `include` folder
+rm -rf ../../../Vorbis/include/
+cp -r jni/include ../../../Vorbis/include
+rm -rf ../../../Vorbis/include/dlg
+# update lib
+rm -rf ../../../Vorbis/clang/$TARGET_ABI/libfreetype.a
+cp -r obj/local/$TARGET_ABI/libvorbis.a ../../../Vorbis/clang/$TARGET_ABI/libvorbis.a
+
+echo "Vorbis build successful"
